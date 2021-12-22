6f76f4cd19057c5a22e9fc98e26895707224a7d0
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2021-12-22 14:26:06 -0300

    firstCommit
    criado projeto no Eclipse e salvo gitperspectiva e commit inicial e push via githubdesktop

---
+++ src/helloworld/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 15;
+		System.out.println("Ola, Mundo!!! " + (a + b) );
+	}
+
+}
+
